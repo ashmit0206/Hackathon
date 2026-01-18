886ad1d4581d63b45936037d577de2eb0b95c01b
meltingliquid <piyushraoc@gmail.com>
2026-01-18 15:05:28 +0530

    all data models

---
+++ backend/models/reminderModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const reminderSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            enum: ["vaccine", "checkup", "medication"],
+            required: true
+        },
+        dueDate: {
+            type: Date,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ["pending", "completed"],
+            default: "pending"
+        }
+    },
+    { timestamps: true }
+);
+
+export const Reminder = mongoose.model("Reminder", reminderSchema);
